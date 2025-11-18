45552d475096e603146b9bfcb339a6e3f926f7b0
quarkusio <no-reply@quarkus.io>
2025-11-18 03:30:54 +0000

    Initial commit

---
+++ src/test/java/br/com/skillmatch/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package br.com.skillmatch;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
